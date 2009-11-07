ac7126fa22436529fe370c5dd8251cbc39a1b758
Sylvain Viollon <thefunny@gmail.com>
2009-11-07 12:46:29 +0100

    Add testing buildout, fix dependencies and egg info.

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(name='zeam.form.autofields',
       description="Automatically collect fields for a Zeam Form.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Framework :: Zope3",
         "Programming Language :: Python",
+        "Programming Language :: Zope",
+        "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='zeam form fields collect',
@@ -18,15 +20,13 @@ setup(name='zeam.form.autofields',
       author_email='thefunny@gmail.com',
       url='',
       license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
       namespace_packages=['zeam', 'zeam.form'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+          'zeam.form.base',
+          ],
       )
